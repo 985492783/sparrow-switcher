2e029b3567488d6356a35b263feb6b244f0ec420
985492783 <985492783@qq.com>
2024-04-08 23:13:06 +0800

    add switch manager

--- switcher-client/src/main/java/com/sparrow/switcher/client/AppSwitch.java
+++ switcher-client/src/main/java/com/sparrow/switcher/client/AppSwitch.java
@@ -1,9 +1,19 @@
 package com.sparrow.switcher.client;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 /**
  * @author 985492783@qq.com
  * @date 2024/4/8 18:24
  */
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface AppSwitch {
-
+    
+    String desc() default "";
 }
---
+++ switcher-client/src/main/java/com/sparrow/switcher/client/SwitcherManager.java
@@ -0,0 +1,17 @@
+package com.sparrow.switcher.client;
+
+/**
+ * switch Manager
+ * @author 985492783@qq.com
+ * @date 2024/4/8 23:11
+ */
+public class SwitcherManager {
+    
+    /**
+     * client register method.
+     * @param clazz class
+     */
+    public static void init(Class<?> clazz) {
+    
+    }
+}
